17d085820e07116d3f68e6b391ce29a2392f2b7d
Jones Hsu <jones.th.hsu@gmail.com>
2020-01-30 15:36:08 +0800

    initialize the project

---
+++ src/test/java/com/joneshsu/great/AppTest.java
@@ -0,0 +1,18 @@
+package com.joneshsu.great;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
